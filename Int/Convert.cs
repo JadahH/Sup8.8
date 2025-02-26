4646aa856c1b6ce991da6372913e1d19b4ff72f3
JadahH <Jhol1918@students.vsu.edu>
2025-02-25 22:04:06 -0500

    Added Person record and Convert class with InitializeData and GetPeopleBornAfter methods.

---
+++ Int/Convert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Int;
+
+    
+public record Person(int Id, string Name, DateTime Birthday);
+public class Convert
+{
+    private Person[] data;
+
+     public void InitializeData()
+        {
+            Random random = new Random();
+            DateTime startDate = new DateTime(1950, 1, 1);
+            int totalDays = (new DateTime(2000, 12, 31) - startDate).Days;
+
+            data = Enumerable.Range(1, 1000000)
+                .Select(i => new Person(
+                    Id: i,
+                    Name: (i % 1000 == 0) ? "Jane Doe" : $"Person_{i}",
+                    Birthday: startDate.AddDays(random.Next(totalDays))
+                ))
+                .ToArray();
+        }
+
+            public Person[] GetPeopleBornAfter(DateTime date) =>
+            data.Where(p => p.Birthday > date).ToArray();
+         
+}
